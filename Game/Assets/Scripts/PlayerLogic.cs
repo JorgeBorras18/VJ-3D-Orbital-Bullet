7a32d501b1c360bf1b6a315db2b242787a574864
JorgeBorras18 <104654988+JorgeBorras18@users.noreply.github.com>
2023-12-23 20:43:06 +0100

    Fixed Input Manager (using InputSystem)

--- Game/Assets/Scripts/PlayerLogic.cs
+++ Game/Assets/Scripts/PlayerLogic.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerLogic : MonoBehaviour
 {
@@ -9,14 +10,23 @@ public class PlayerLogic : MonoBehaviour
     public Animation_Controller animationController;
     Angular_Physics angularPhysics;
     public BoxCollider boxCollider;
+    public PlayerInput _playerInput;
 
+    // PHYSISCS VALUES
     public float moveSpeed = 5f, jumpSpeed = 10f, rollSpeed = 10f, slow_fall_gravity = 0.45f, fast_fall_gravity = 0.7f;
     public float radiusRing = 9f;
     private bool isThereWallAhead = false;
 
-    // JUMP LOGIC
-    private bool doubled_jumped_already = false;
+    //INPUT VALUES
+    private InputAction MoveAction;
+    private InputAction RollAction;
+    private bool roll_was_pressed = false;
+    private InputAction JumpAction;
     private bool up_was_pressed = false;
+    private InputAction CrouchAction;
+
+    // MOVEMENT STAT VARIABLES LOGIC
+    private bool doubled_jumped_already = false;
     private bool up_is_still_pressed = false;
     private bool facingRight = false;
 
@@ -27,12 +37,20 @@ public class PlayerLogic : MonoBehaviour
         animationController = GameObject.Find("Player_Animation_Controller").gameObject.GetComponent<Animation_Controller>();
         angularPhysics = GetComponent<Angular_Physics>();
         angularPhysics.init(radiusRing, 3f * Mathf.PI / 4f);
+
+        //load input watchers
+        _playerInput = GetComponent<PlayerInput>();
+        MoveAction = _playerInput.actions["Move"];
+        RollAction = _playerInput.actions["Roll"];
+        JumpAction = _playerInput.actions["Jump"];
+        CrouchAction = _playerInput.actions["Crouch"];
     }
 
     // Catch 
     private void Update()
     {
-        up_was_pressed = up_was_pressed || (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow));
+        up_was_pressed = up_was_pressed || JumpAction.WasPressedThisFrame();
+        roll_was_pressed = roll_was_pressed || RollAction.WasPressedThisFrame();
     }
 
     // Physics Update is called once every 0.02 seconds
@@ -42,6 +60,7 @@ public class PlayerLogic : MonoBehaviour
         float selected_gravity = fast_fall_gravity;
         float step = 0;
         string next_Animation = "Jump";
+        float movementInput = MoveAction.ReadValue<Vector2>().x;
 
         //Check animation
         if (animationController.getActualState() == "Roll" && !animationController.animationHasFinished())
@@ -80,13 +99,13 @@ public class PlayerLogic : MonoBehaviour
             if (angularPhysics.getVerticalSpeed() <= jumpSpeed * 0.4f) next_Animation = "Air_Fall";
             if (up_is_still_pressed)
             {
-                if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))) selected_gravity = slow_fall_gravity;
+                if (JumpAction.IsPressed()) selected_gravity = slow_fall_gravity;
                 else up_is_still_pressed = false;
             }
         }
 
         // HORIZONTAL MOVEMENT
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (movementInput == -1f)
         {
             step = -moveSpeed;
             if (facingRight == true)
@@ -99,7 +118,7 @@ public class PlayerLogic : MonoBehaviour
             if (controller.isGrounded) next_Animation = "Run";
         }
 
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        else if (movementInput == 1f)
         {
             step = moveSpeed;
             if (facingRight == false)
@@ -113,17 +132,22 @@ public class PlayerLogic : MonoBehaviour
         }
         else if (controller.isGrounded)
         {
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) next_Animation = "Crouch";
+            if (CrouchAction.IsPressed()) next_Animation = "Crouch";
             else next_Animation = "Idle";
         }
-        if (Input.GetKey(KeyCode.Z)) next_Animation = "Roll";
+        if (roll_was_pressed)
+        {
+            next_Animation = "Roll";
+            roll_was_pressed = false;
+        }
+
 
         // UPDATE PLAYER MOVEMENT & ANIMATION
         angularPhysics.moveObject(step, selected_gravity);
         animationController.changeAnimation(next_Animation);
 
         // KILL PLAYER
-        if (Input.GetKey(KeyCode.X)) animationController.setAlive(false);
+        //if (Input.GetKey(KeyCode.X)) animationController.setAlive(false);
     }
 
     private void OnTriggerEnter(Collider hit)
